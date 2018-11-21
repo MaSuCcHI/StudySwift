c54a850e9416f1c62aa7c4fde2c70025a9da0815
koko <keiske.masuda@gmai.com>
2018-11-21 21:26:35 +0900

    Initial Commit

---
+++ StudySwiftTests/StudySwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  StudySwiftTests.swift
+//  StudySwiftTests
+//
+//  Created by 舛田圭丞 on 2018/11/21.
+//  Copyright © 2018 舛田圭丞. All rights reserved.
+//
+
+import XCTest
+@testable import StudySwift
+
+class StudySwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
